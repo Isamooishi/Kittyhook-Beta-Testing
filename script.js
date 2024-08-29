38fa8b15e1075106568f79e49ad89a33acd495af
Isamooishi <famlyonly697@gmail.com>
2024-08-29 12:04:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var userlist = new Userlist();
+
+userlist.users.create({
+    identifier: user.id,
+    email: user.email,
+    properties: {
+        first_name: user.first_name,
+        last_name: user.last_name
+
+
+        userlist.users.delete(user.id)
+
+
+    }
+});
